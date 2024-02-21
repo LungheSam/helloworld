8e8962b75a4901931cb2d43a808620b0f2cc80dd
Samuel Lunghe <samuellunghe@gmail.com>
2024-02-21 15:55:39 +0300

    commit-4

--- src/gitpython.py
+++ src/gitpython.py
@@ -2,25 +2,48 @@
 import time
 import pyttsx3
 
-def text_to_speech(text):
-    # Initialize the text-to-speech engine
-    engine = pyttsx3.init()
+# def text_to_speech(text):
+#     # Initialize the text-to-speech engine
+#     engine = pyttsx3.init()
 
-    # Set properties (optional)
-    # engine.setProperty('rate', 150)  # Speed of speech
-    # engine.setProperty('volume', 0.9)  # Volume level (0.0 to 1.0)
+#     # Set properties (optional)
+#     # engine.setProperty('rate', 150)  # Speed of speech
+#     # engine.setProperty('volume', 0.9)  # Volume level (0.0 to 1.0)
 
-    # Convert text to speech
-    engine.say(text)
+#     voices = engine.getProperty('voices')
+#     # print(voices)
+#     engine.setProperty('voice', voices[0].id)
 
-    # Wait for the speech to finish
-    engine.runAndWait()
+#     # Convert text to speech
+#     engine.say(text)
 
-# Example usage:
-text = "Hello, how are you today?"
-text_to_speech(text)
+#     # Wait for the speech to finish
+#     engine.runAndWait()
+# # text_to_speech("Hello, How are you doing today?")
+class Point:
+    def __init__(self,x,y,z=0):
+        self.x=x
+        self.y=y
+        self.z=z
+    def __repr__(self):
+        return f"x:{self.x}\ny:{self.y}\nz:{self.z}"
+    def describe(self):
+        print(f"Point ({self.x},{self.y},{self.z})\nx:{self.x}\ny:{self.y}\nz:{self.z}")
+    def say_coordinates(self):
+        engine=pyttsx3.init()
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[2].id)
+        engine.setProperty("rate",150)
+        engine.say(f"coordinates of the point are, x,:{self.x}\ny,:{self.y}\nz,:{self.z}")
+        engine.runAndWait()
+    def __add__(self,other):
+        return (self.x+other.x,self.y+other.y,self.z+other.z)
+    def __sub__(self,other):
+        return (self.x-other.x,self.y-other.y,self.z-other.z)
 
-# for i in range(30,1,-1):
-#     # time.sleep(1)
-#     i=i if i>=10 else "0"+str(i)
-#     print(i)
+A=Point(2,4)
+B=Point(1,2)
+A.describe()
+B.describe()
+A.say_coordinates()
+print(A+B)
